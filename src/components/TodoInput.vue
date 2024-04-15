80a8719f4b73651f495610a36b3e6525b747f3f1
BancaKim <weberian92@gmail.com>
2024-04-15 23:11:11 +0900

    vue-inter

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <button @click="addTodo">add</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                newTodoItem: ""
+            }
+        },
+        methods:{
+            addTodo() {
+                //저장하는 로직
+                localStorage.setItem(this.newTodoItem, this.newTodoItem);
+                this.newTodoItem="";
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
